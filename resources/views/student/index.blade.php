ec821c4890b86da05433537d4da4ec5e0332dd75
Alex Hernandez <133897289+AlexHernandez2698632494@users.noreply.github.com>
2024-06-07 10:33:16 -0600

    actualizacion

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -1,11 +1,11 @@
 @extends('layout.header')
 
-@section('title','Login')
+@section('title', 'Login')
 
 <script src="{{ asset('js/sweetalert.js') }}"></script>
 {{-- #7facff --}}
 <body>
-	@if (session('errorLogin'))
+    @if (session('errorLogin'))
         <script>
             swal({
                 title: "Información incorrecta",
@@ -15,17 +15,17 @@
             })            
         </script>
     @endif
-	@if (session('exitoRegistoAdmin'))
+    @if (session('exitoRegistroAdmin'))
         <script>
             swal({
                 title: "Administrador registrado",
-                text: "{{ session('exitoRegistoAdmin') }}",
+                text: "{{ session('exitoRegistroAdmin') }}",
                 icon: "success",
                 button: "OK",
             })            
         </script>
     @endif
-	@if (session('exitoAgregar'))
+    @if (session('exitoAgregar'))
         <script>
             swal({
                 title: "Invitado registrado",
@@ -35,7 +35,7 @@
             })            
         </script>
     @endif
-	@if (session('exitoSolicitud'))
+    @if (session('exitoSolicitud'))
         <script>
             swal({
                 title: "Solicitud realizada",
@@ -45,35 +45,33 @@
             })            
         </script>
     @endif
-<body>
-	<section class="vh-100" style="background-color: #0060B4;">
-		<div class="container py-5 h-100">
-			<div class="row d-flex justify-content-center align-items-center h-100">
-				<div class="col-12 col-md-8 col-lg-6 col-xl-5">
-					<div class="card shadow-2-strong" style="border-radius: 1rem;">
-						<div class="card-body p-5 text-center">
-							<h3 class="mb-1">¡Bienvenido!</h3>
-							<h3 class="mb-3">Sistema de Registro de Asistencia DAC</h3>
-							<img class="img" width="125" src="http://127.0.0.1:8000/img/LOGO-DAC-20214.png" alt="LOGO DE DEPARTAMENTO DE ARTE Y CULTURA (DAC)">
+    <section class="vh-100" style="background-color: #0060B4;">
+        <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                    <div class="card shadow-2-strong" style="border-radius: 1rem;">
+                        <div class="card-body p-5 text-center">
+                            <h3 class="mb-1">¡Bienvenido!</h3>
+                            <h3 class="mb-3">Sistema de Registro de Asistencia DAC</h3>
+                            <img class="img" width="125" src="http://127.0.0.1:8000/img/LOGO-DAC-20214.png" alt="LOGO DEL DEPARTAMENTO DE ARTE Y CULTURA (DAC)">
                             <!-- Pills navs -->
-							<ul class="nav nav-pills nav-justified mb-3" id="ex1" role="tablist">
-							<li class="nav-item" role="presentation">
-								<a href="{{route('UDBStudentGuestSite.index') }}" class="nav-link" id="tab-register" data-mdb-toggle="pill" role="tab" aria-controls="pills-register" aria-selected="false" style="color: #0060B4;">Registrar Estudiante UDB</a>
-							</li>
-								<li class="nav-item" role="presentation">
-									<a href="{{route('UDBStaffGuestSite.index')}}" class="nav-link" id="tab-register" data-mdb-toggle="pill"  role="tab" aria-controls="pills-register" aria-selected="false" style="color: #0060B4;">Registrar Personal UDB</a>
-								</li>
-							</ul>
+                            <ul class="nav nav-pills nav-justified mb-3" id="ex1" role="tablist">
+                                <li class="nav-item" role="presentation">
+                                    <a href="{{route('UDBStudentGuestSite.index') }}" class="nav-link" id="tab-register" data-mdb-toggle="pill" role="tab" aria-controls="pills-register" aria-selected="false" style="color: #0060B4;">Registrar Estudiante UDB</a>
+                                </li>
+                                <li class="nav-item" role="presentation">
+                                    <a href="{{route('UDBStaffGuestSite.index')}}" class="nav-link" id="tab-register" data-mdb-toggle="pill" role="tab" aria-controls="pills-register" aria-selected="false" style="color: #0060B4;">Registrar Personal UDB</a>
+                                </li>
+                            </ul>
                             <div class="row">
                                 <a href="{{route('welcome')}}" class="btn btn-primary btn-lg btn-block mt-2" style="background-color: #0060B4;" type="submit">Regresar</a>
                             </div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		</div>
-	</section>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </body>
 
 </html>
