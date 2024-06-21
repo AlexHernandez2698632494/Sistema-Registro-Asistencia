64e3fea6e14fff4419f54fbb3b1b43a50e064169
DanielMenjivar20 <dani20.menjivar@gmail.com>
2024-06-21 14:00:53 -0600

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,7 +40,17 @@
 			<div class="row d-flex justify-content-center align-items-center h-100">
 				<div class="col-12 col-md-8 col-lg-6 col-xl-5">
 					<div class="card shadow-2-strong" style="border-radius: 1rem;">
-						<div class="card-body p-5 text-center">			
+						<div class="card-body p-5 text-center">	
+							<!-- Pills navs -->
+							<ul class="nav nav-pills nav-justified mb-3" id="ex1" role="tablist">
+							<li class="nav-item" role="presentation">
+								<a href="{{route('student.guest') }}" class="nav-link" id="tab-register" data-mdb-toggle="pill" role="tab" aria-controls="pills-register" aria-selected="false" style="color: #0060B4;">Registrar Invitado</a>
+							</li>
+								<li class="nav-item" role="presentation">
+									<a href="{{route('student.index')}}" class="nav-link" id="tab-register" data-mdb-toggle="pill"  role="tab" aria-controls="pills-register" aria-selected="false" style="color: #0060B4;">Registrar UDB</a>
+								</li>
+							</ul>
+							<!-- Pills navs -->		
 							<h3 class="mb-1">¡Bienvenido!</h3>
 							<h3 class="mb-3">Sistema de Registro de Asistencia DAC</h3>
 							<img class="img" width="125" src="http://127.0.0.1:8000/img/LOGO-DAC-2021.png" alt="">
