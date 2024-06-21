82a801385e8a52a9dff05ffa8685c9ca62e1249d
Alex Hernandez <133897289+AlexHernandez2698632494@users.noreply.github.com>
2024-06-21 13:51:07 -0600

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.header')
 
 @section('title','Login')
-
+    
 <script src="{{ asset('js/sweetalert.js') }}"></script>
 {{-- #7facff --}}
 <body>
@@ -15,93 +15,74 @@
             })            
         </script>
     @endif
-	@if (session('exitoRegistroAdmin'))
+	@if (session('exitoRegistoAdmin'))
         <script>
             swal({
                 title: "Administrador registrado",
-                text: "{{ session('exitoRegistroAdmin') }}",
+                text: "{{ session('exitoRegistoAdmin') }}",
                 icon: "success",
                 button: "OK",
             })            
         </script>
     @endif
-	@if (session('exitoAgregar'))
+	@if (session('exitoSolicitud'))
         <script>
             swal({
-                title: "Invitado registrado",
-                text: "{{ session('exitoAgregar') }}",
+                title: "Solicitud realizada",
+                text: "{{ session('exitoSolicitud') }}",
                 icon: "success",
                 button: "OK",
             })            
         </script>
     @endif
-	@if (session('exitoRestablecer'))
-	<script>
-		swal({
-			title: "Contraseña restablecida",
-			text: "{{ session('exitoRestablecer') }}",
-			icon: "success",
-			button: "OK",
-		})
-	</script>
-@endif
-
-@if (session('errorRestablecer'))
-	<script>
-		swal({
-			title: "Error al restablecer",
-			text: "{{ session('errorRestablecer') }}",
-			icon: "error",
-			button: "OK",
-		})            
-	</script>
-@endif
-<body>
-	<section class="vh-200" style="background-color: #0060B4;">
-		<div class="container py-5 h-200">
-			<div class="row d-flex justify-content-center align-items-center h-200">
+   <section class="vh-100" style="background-color: #0060B4;">
+		<div class="container py-5 h-100">
+			<div class="row d-flex justify-content-center align-items-center h-100">
 				<div class="col-12 col-md-8 col-lg-6 col-xl-5">
 					<div class="card shadow-2-strong" style="border-radius: 1rem;">
-						<div class="card-body p-5 text-center">
-							<!-- Pills navs -->
-							<ul class="nav nav-pills nav-justified mb-3" id="ex1" role="tablist">
-							<li class="nav-item" role="presentation">
-								<a href="{{route('student.guest') }}" class="nav-link" id="tab-register" data-mdb-toggle="pill" role="tab" aria-controls="pills-register" aria-selected="false" style="color: #0060B4;">Registrar Invitado</a>
-							</li>
-								<li class="nav-item" role="presentation">
-									<a href="{{route('student.index')}}" class="nav-link" id="tab-register" data-mdb-toggle="pill"  role="tab" aria-controls="pills-register" aria-selected="false" style="color: #0060B4;">Registrar UDB</a>
-								</li>
-							</ul>
-							<!-- Pills navs -->
-
+						<div class="card-body p-5 text-center">			
 							<h3 class="mb-1">¡Bienvenido!</h3>
 							<h3 class="mb-3">Sistema de Registro de Asistencia DAC</h3>
-							<img class="img" width="125" src="http://127.0.0.1:8000/img/LOGO-DAC-2021.png" alt="LOGO DE DEPARTAMENTO DE ARTE Y CULTURA (DAC)">
-							<div class="my-2" style="background-color:#0060B4; "></div>
-							<form method="post" action="{{ route('login') }}">
-								@csrf
-								<div class="input-group mb-3">
-									<span class="input-group-text" id="basic-addon1"><i class="fa-solid fa-user"></i></span>
-									<input type="text" class="form-control" placeholder="Usuario" aria-label="user" name="user">
-								</div>
-								<div class="input-group mb-3">
-									<span class="input-group-text" id="basic-addon1"><i class="fa-solid fa-lock"></i></span>
-									<input type="password" class="form-control" placeholder="Contraseña" aria-label="password" name="password">
-								</div>
-								<div class="row">
-									<button class="btn btn-primary btn-lg btn-block mt-2" style="background-color: #0060B4;" type="submit">Ingresar</button>
-								</div>
-								<div class="row">
-									<p class="mt-3"><a href="{{ route('recuperarView') }}" class="link-underline-primary">Olvidé mi contraseña</a></p>
-								</div>
-							</form>
+							<img class="img" width="125" src="http://127.0.0.1:8000/img/LOGO-DAC-2021.png" alt="">
+															
+							<div class="my-4" style="background-color:#0060B4; height: 3px; border-radius: 2rem"></div>
+								@if ($errors->any())
+									<div class="alert alert-danger my-2 pb-0">
+										<ul>
+											@foreach ($errors->all() as $error)
+												<li>{{ $error }}</li>
+											@endforeach
+										</ul>
+									</div>
+								@endif
+								@if(session('error'))
+									<div class="alert alert-danger">
+										{{ session('error') }}
+									</div>
+								@endif
+								<form method="POST" action="{{ route('login') }}">
+									@csrf
+									<div class="input-group mb-3">
+										<span class="input-group-text" id="basic-addon1"><i class="fa-solid fa-user"></i></span>
+										<input type="text" class="form-control" placeholder="Usuario" aria-label="user" name="user">
+									</div>
+									<div class="input-group mb-3">
+										<span class="input-group-text" id="basic-addon1"><i class="fa-solid fa-lock"></i></span>
+										<input type="password" class="form-control" placeholder="Contraseña" aria-label="password" name="password">
+									</div>	
+									<div class="row">
+										<button class="btn btn-primary btn-lg btn-block mt-2" type="submit">Ingresar</button>
+									</div>	
+									<div class="row">
+										<p class="mt-3"><a href="{{ route('recuperarView') }}" class="link-underline-primary">Olvidé mi contraseña</a></p>
+									</div>											
+								</form>		
+							</div>										
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-		</div>
-	</section>
+   </section>    
 </body>
-
 </html>
